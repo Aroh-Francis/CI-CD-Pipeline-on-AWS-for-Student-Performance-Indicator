8ccc8247c193c0225ce757004c1baf2e2b6a7f9c
Francis Aroh <arohfrancisco@gmail.com>
2024-12-30 22:57:10 +0000

    AWS Deployment Configuration

--- src/utils.py
+++ src/utils.py
@@ -63,7 +63,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param): #
 
 
 
- #Markout from here:
+
 def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
